551f7f32eb6dce4ddff54efae0f558b68c785919
licop <licop@users.noreply.github.com>
2021-03-28 23:51:45 +0800

    update

--- src/TinyReact/updateNodeElement.js
+++ src/TinyReact/updateNodeElement.js
@@ -1,20 +1,47 @@
-export default function updateNodeElement(newElement, virtualDOM) {
+export default function updateNodeElement(newElement, virtualDOM, oldVirtualDOM = {}) {
   // 获取节点对应的属性对象
-  const newProps = virtualDOM.props
+  const newProps = virtualDOM.props || {}
+  const oldProps = oldVirtualDOM.props || {}
+
   Object.keys(newProps).forEach(propsName => {
     const newPropsValue = newProps[propsName]
-    // 判断属性是否是事件属性 onClick => click
-    if(propsName.slice(0, 2) === 'on') {
-      const eventName = propsName.toLowerCase().slice(2)
-      newElement.addEventListener(eventName, newPropsValue)
-    } else if(propsName === 'value' || propsName === 'checked') {
-      newElement[propsName] = newPropsValue
-    } else if(propsName !== 'children') {
-      if(propsName === "className") {
-        newElement.setAttribute('class', newPropsValue)
-      } else {
-        newElement.setAttribute(propsName, newPropsValue)
+    const oldPropsValue = oldProps[propsName]
+    if(newPropsValue !== oldPropsValue) {
+      // 判断属性是否是事件属性 onClick => click
+      if(propsName.slice(0, 2) === 'on') {
+        // 事件名称
+        const eventName = propsName.toLowerCase().slice(2)
+        // 为元素添加事件
+        newElement.addEventListener(eventName, newPropsValue)
+        // 删除原有的事件的事件处理函数
+        if(oldPropsValue) {
+          newElement.removeEventListener(eventName, oldPropsValue)
+        }
+
+      } else if(propsName === 'value' || propsName === 'checked') {
+        newElement[propsName] = newPropsValue
+      } else if(propsName !== 'children') {
+        if(propsName === "className") {
+          newElement.setAttribute('class', newPropsValue)
+        } else {
+          newElement.setAttribute(propsName, newPropsValue)
+        }
+      } 
+    }
+  })
+  // 判断属性被删除的情况
+  Object.keys(oldProps).forEach(propsName => {
+    const newPropsValue = newProps[propsName]
+    const oldPropsValue = oldProps[propsName]
+
+    if(!newPropsValue) {
+      // 属性被删除了
+      if(propsName.slice(0, 2) === 'on') {
+        const eventName = propsName.toLowerCase().slice(2)
+        newElement.removeEventListener(eventName, oldPropsValue)
+      } else if(propsName !== 'children') {
+        newElement.removeAttritube(propName)
       }
-    } 
+    }
   })
 }
